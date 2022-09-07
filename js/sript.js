41c0dd0460fad659dd831d6b034cc7c9072d95ff
Umang Rathod <87117456+rockypagal@users.noreply.github.com>
2022-09-07 14:45:38 +0530

    Update sript.js

--- js/sript.js
+++ js/sript.js
@@ -5,3 +5,18 @@ menu.addEventListener('click',function () {
     link.classList.toggle('show');
     this.classList.toggle('cross');
 })
+
+menu.addEventListener('click',function () {
+    if(link.classList.contains('show')){
+    scrollTop = document.documentElement.scrollTop;
+    scrollLeft =document.documentElement.scrollLeft;
+  
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+else{
+    window.onscroll = function() {};
+}
+});
